6fac92cb82bc523af16547b0dfb2dc3627c1b1ef
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-04-18 15:16:57 +0100

    initial commit

---
+++ src/main/java/com/example/cinema_spring_lab/CinemaSpringLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_spring_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSpringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaSpringLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cinema_spring_lab/CinemaSpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinema_spring_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSpringLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
